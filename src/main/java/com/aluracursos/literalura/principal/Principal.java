ee0ce75d6af50281359504b4c6c0aca28b9e0c75
Alb3rtoGitHub <ravallecillo@hotmail.com>
2024-10-28 16:09:28 -0300

    Modelado de Objetos Autor y Libro, relacion entre clases, tablas.

--- src/main/java/com/aluracursos/literalura/principal/Principal.java
+++ src/main/java/com/aluracursos/literalura/principal/Principal.java
@@ -20,67 +20,69 @@ public class Principal {
         var data = conversor.obtenerDatos(json, Datos.class);
         System.out.println(data);
 
+        var opcion = -1;
 
+        while (opcion != 0) {
+            var menu = """
+                    **************************************************
+                    *                 ~ LITERALURA ~                 *
+                    **************************************************
+                    *                 MENU PRINCIPAL                 *
+                    **************************************************
+                    1 - Buscar Libro por Titulo
+                    2 - Listar Libros Registrados
+                    3 - Listar Autores Registrados
+                    4 - Listar Autores vivos en un determinado año
+                    5 - Listar Libros por Idioma
 
-//        var opcion = -1;
-//
-//        while (opcion != 0) {
-//            var menu = """
-//                    **************************************************
-//                    *                 ~ LITERALURA ~                 *
-//                    **************************************************
-//                    *                 MENU PRINCIPAL                 *
-//                    **************************************************
-//                    1 - Buscar Libro por Titulo
-//                    2 - Listar Libros Registrados
-//                    3 - Listar Autores Registrados
-//                    4 - Listar Autores vivos en un determinado año
-//                    5 - Listar Libros por Idioma
-//
-//                    0 - Salir
-//                    """;
-//            System.out.println(menu);
-//            System.out.print("Opcion: ");
-//            opcion = sc.nextInt();
-//            sc.nextLine();
-//            opcion = 0;
-//
-//            switch (opcion) {
-////                case 1:
-////                    buscarLibroPorTitulo();
-////                    break;
-////                case 2:
-////                    buscarEpisodioPorSerie();
-////                    break;
-////                case 3:
-////                    mostrarSeriesBuscadas();
-////                    break;
-////                case 4:
-////                    buscarSeriesPorTitulo();
-////                    break;
-////                case 5:
-////                    buscarTop5Series();
-////                    break;
-////                case 6:
-////                    buscarSeriesPorCategoria();
-////                    break;
-////                case 7:
-////                    filtrarSeriesPorTemporadaYEvaluacion();
-////                    break;
-////                case 8:
-////                    buscaEpisodiosPorTitulo();
-////                    break;
-////                case 9:
-////                    buscarTop5Espisodios();
-////                    break;
-//
-//                case 0:
-//                    System.out.println("Cerrando la aplicación...");
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            System.out.print("Opcion: ");
+            String opcionMenu = sc.nextLine();
+
+            try {
+                opcion = Integer.parseInt(opcionMenu);
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor ingrese una Opción válida [numero entero].");
+            }
+
+            switch (opcion) {
+//                case 1:
+//                    buscarLibroPorTitulo();
 //                    break;
-//                default:
-//                    System.out.println("Opción inválida");
-//            }
+//                case 2:
+//                    buscarEpisodioPorSerie();
+//                    break;
+//                case 3:
+//                    mostrarSeriesBuscadas();
+//                    break;
+//                case 4:
+//                    buscarSeriesPorTitulo();
+//                    break;
+//                case 5:
+//                    buscarTop5Series();
+//                    break;
+//                case 6:
+//                    buscarSeriesPorCategoria();
+//                    break;
+//                case 7:
+//                    filtrarSeriesPorTemporadaYEvaluacion();
+//                    break;
+//                case 8:
+//                    buscaEpisodiosPorTitulo();
+//                    break;
+//                case 9:
+//                    buscarTop5Espisodios();
+//                    break;
 
-//        }
+                case 0:
+                    System.out.println("Cerrando la aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+
+        }
     }
 }
